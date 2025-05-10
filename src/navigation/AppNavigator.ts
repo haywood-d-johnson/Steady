e33087def2e762d80da04f0326581072a8a71669
Haywood D. Johnson <haywood.d.johnson@gmail.com>
2025-05-10 01:35:39 -0500

    feat: beginning navigation changes

---
+++ src/navigation/AppNavigator.ts
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//import HomeScreen from "../screens/HomeScreen";
+//import DetailsScreen from "../screens/DetailsScreen";
+
+const stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
